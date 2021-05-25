2e1d53d96d43aec01db3400299421e9f14c30f90
Kay N <kaynguyen.dev@gmail.com>
2021-05-25 11:22:12 -0400

    tweaked some page/component types

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,16 +6,21 @@ const defaultKeywords = "";
 const defaultOGURL = "";
 const defaultOGImage = "";
 
-const Head = (props) => (
+type HeadProps = {
+  title: string;
+  description?: string;
+  keywords?: string;
+  url?: string;
+  ogImage?: string;
+};
+
+const Head = ({ title, description, keywords, url, ogImage }: HeadProps) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ""}</title>
+    <title>{title || ""}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="keywords" content={props.keywords || defaultKeywords} />
+    <meta name="description" content={description || defaultDescription} />
+    <meta name="keywords" content={keywords || defaultKeywords} />
     <link
       rel="icon"
       type="image/png"
@@ -35,27 +40,19 @@ const Head = (props) => (
       href="/static/apple-touch-icon.png"
     />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#000000" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ""} />
+    <meta property="og:url" content={url || defaultOGURL} />
+    <meta property="og:title" content={title || ""} />
     <meta
       property="og:description"
-      content={props.description || defaultDescription}
+      content={description || defaultDescription}
     />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
+    <meta name="twitter:site" content={url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    <meta name="twitter:image" content={ogImage || defaultOGImage} />
+    <meta property="og:image" content={ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
 );
-
-Head.propTypes = {
-  title: string,
-  description: string,
-  keywords: string,
-  url: string,
-  ogImage: string,
-};
 
 export default Head;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import AppProps from "next/app";
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
