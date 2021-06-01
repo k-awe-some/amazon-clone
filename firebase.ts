592025c61b3798a9b4f2f78628478ab10d2885e9
Kay N <kaynguyen.dev@gmail.com>
2021-06-01 08:29:42 -0400

    set up firebase

---
+++ firebase.ts
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf7qikAzGVJzc5dNkGRRD4mQrkD751BvM",
+  authDomain: "clone-b733b.firebaseapp.com",
+  projectId: "clone-b733b",
+  storageBucket: "clone-b733b.appspot.com",
+  messagingSenderId: "635156921350",
+  appId: "1:635156921350:web:11f943c203b1809fdf5787",
+};
